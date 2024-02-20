a830955409d44d79101a4f34547ad369d4a77015
Vi Retault <g33kex@pm.me>
2024-02-19 19:24:55 -0500

    refactored the tests

--- tests/test_pyqrypto.py
+++ tests/test_pyqrypto.py
@@ -1,23 +1,27 @@
-"""Test pyqrypto"""
-from qiskit import QuantumCircuit, QuantumRegister, AncillaRegister
+"""Test pyqrypto."""
+from __future__ import annotations
+
+import random
+from itertools import chain
+from typing import Callable
+
 from pyqrypto.register_operations import (
+    RegisterCircuit,
+    RegisterConstantDKRSCarryLookaheadAdder,
+    RegisterDKRSCarryLookaheadAdder,
     make_circuit,
     run_circuit,
-    RegisterCircuit,
-    RegisterDKRSCarryLookaheadAdder,
-    RegisterConstantDKRSCarryLookaheadAdder,
 )
 from pyqrypto.sparkle import (
     Alzette,
-    Traxl_enc,
-    Traxm_enc_round,
+    TraxlEnc,
+    TraxmEncRound,
     c_alzette,
-    c_traxl_genkeys,
     c_traxl_enc,
+    c_traxl_genkeys,
     c_traxm_enc,
 )
-from itertools import chain
-import random
+from qiskit import AncillaRegister, QuantumCircuit, QuantumRegister
 
 NB_TESTS = 100
 """Number of times to run each test"""
@@ -28,21 +32,28 @@ METHOD = "matrix_product_state"
 
 
 # Utils
-def rol(x, r, n):
+def rol(x: int, r: int, n: int) -> int:
+    """Left rotation."""
     r = r % n
     return ((x << r) | (x >> (n - r))) & ((2**n) - 1)
 
 
-def ror(x, r, n):
+def ror(x: int, r: int, n: int) -> int:
+    """Right rotation."""
     r = r % n
     return ((x >> r) | (x << (n - r))) & ((2**n) - 1)
 
 
 # Generic circuit test
 def circuit_test(
-    circuit, classical_function, inputs, input_registers, output_registers, verbose=False
-):
-    """Tests if a circuit's output corresponds to the classical function's output"""
+    circuit: RegisterCircuit,
+    classical_function: Callable,
+    inputs: list[int],
+    input_registers: list[QuantumRegister],
+    output_registers: list[QuantumRegister],
+    verbose=False,
+) -> bool:
+    """Test if a circuit's output corresponds to the classical function's output."""
     true_result = classical_function(*inputs)
 
     if verbose:
@@ -66,24 +77,26 @@ def circuit_test(
 
 
 # Specific tests
-def test_prepare():
+def test_prepare() -> None:
+    """Test circuit preparation."""
     for _ in range(NB_TESTS):
-        n1 = random.randint(1, 10)
-        n2 = random.randint(1, 10)
-        a = random.getrandbits(n1)
-        b = random.getrandbits(n2)
+        n_1 = random.randint(1, 10)
+        n_2 = random.randint(1, 10)
+        a = random.getrandbits(n_1)
+        b = random.getrandbits(n_2)
 
-        X = QuantumRegister(n1)
-        Y = QuantumRegister(n2)
+        X = QuantumRegister(n_1)
+        Y = QuantumRegister(n_2)
 
-        qc = QuantumCircuit(X, Y)
+        circuit = QuantumCircuit(X, Y)
 
-        result = circuit_test(qc, lambda x, y: [x, y], [a, b], [X, Y], [X, Y])
+        result = circuit_test(circuit, lambda x, y: [x, y], [a, b], [X, Y], [X, Y])
 
         assert result
 
 
-def test_ror():
+def test_ror() -> None:
+    """Test right rotation."""
     for _ in range(NB_TESTS):
         n = random.randint(1, 10)
         a = random.getrandbits(n)
@@ -91,15 +104,16 @@ def test_ror():
 
         X1 = QuantumRegister(n)
 
-        qc = RegisterCircuit(X1)
-        X2 = qc.ror(X1, r)
+        circuit = RegisterCircuit(X1)
+        X2 = circuit.ror(X1, r)
 
-        result = circuit_test(qc, lambda x: [ror(x, r, n)], [a], [X1], [X2])
+        result = circuit_test(circuit, lambda x: [ror(x, r, n)], [a], [X1], [X2])
 
         assert result
 
 
-def test_xor():
+def test_xor() -> None:
+    """Test xor between two quantum registers."""
     for _ in range(NB_TESTS):
         n = random.randint(1, 10)
         a = random.getrandbits(n)
@@ -108,15 +122,16 @@ def test_xor():
         X = QuantumRegister(n)
         Y = QuantumRegister(n)
 
-        qc = RegisterCircuit(X, Y)
-        qc.xor(X, Y)
+        circuit = RegisterCircuit(X, Y)
+        circuit.xor(X, Y)
 
-        result = circuit_test(qc, lambda x, y: [x ^ y], [a, b], [X, Y], [X])
+        result = circuit_test(circuit, lambda x, y: [x ^ y], [a, b], [X, Y], [X])
 
         assert result
 
 
-def test_xorc():
+def test_xorc() -> None:
+    """Test xor with a constant."""
     for _ in range(NB_TESTS):
         n = random.randint(1, 10)
         a = random.getrandbits(n)
@@ -124,15 +139,16 @@ def test_xorc():
 
         X1 = QuantumRegister(n)
 
-        qc = RegisterCircuit(X1)
-        X2 = qc.xor(X1, c)
+        circuit = RegisterCircuit(X1)
+        X2 = circuit.xor(X1, c)
 
-        result = circuit_test(qc, lambda x: [x ^ c], [a], [X1], [X2])
+        result = circuit_test(circuit, lambda x: [x ^ c], [a], [X1], [X2])
 
         assert result
 
 
-def test_rorrorxor():
+def test_rorrorxor() -> None:
+    """Test chaining ror, ror and xor."""
     for _ in range(NB_TESTS):
         n = random.randint(1, 10)
         a = random.getrandbits(n)
@@ -143,19 +159,24 @@ def test_rorrorxor():
         X1 = QuantumRegister(n)
         Y1 = QuantumRegister(n)
 
-        qc = RegisterCircuit(X1, Y1)
-        X2 = qc.ror(X1, r1)
-        Y2 = qc.ror(Y1, r2)
-        qc.xor(X2, Y2)
+        circuit = RegisterCircuit(X1, Y1)
+        X2 = circuit.ror(X1, r1)
+        Y2 = circuit.ror(Y1, r2)
+        circuit.xor(X2, Y2)
 
         result = circuit_test(
-            qc, lambda x, y: [ror(x, r1, n) ^ ror(y, r2, n), y], [a, b], [X1, Y1], [X2, Y1]
+            circuit,
+            lambda x, y: [ror(x, r1, n) ^ ror(y, r2, n), y],
+            [a, b],
+            [X1, Y1],
+            [X2, Y1],
         )
 
         assert result
 
 
-def test_rorxorrolxor():
+def test_rorxorrolxor() -> None:
+    """Test chaining ror, xor, rol and xor."""
     for _ in range(NB_TESTS):
         n = random.randint(1, 10)
         a = random.getrandbits(n)
@@ -166,13 +187,13 @@ def test_rorxorrolxor():
         X1 = QuantumRegister(n)
         Y1 = QuantumRegister(n)
 
-        qc = RegisterCircuit(X1, Y1)
-        X2 = qc.xor(qc.ror(X1, r1), Y1)
-        Y2 = qc.rol(Y1, r2)
-        qc.xor(X2, Y2)
+        circuit = RegisterCircuit(X1, Y1)
+        X2 = circuit.xor(circuit.ror(X1, r1), Y1)
+        Y2 = circuit.rol(Y1, r2)
+        circuit.xor(X2, Y2)
 
         result = circuit_test(
-            qc,
+            circuit,
             lambda x, y: [ror(x, r1, n) ^ y ^ rol(y, r2, n), rol(y, r2, n)],
             [a, b],
             [X1, Y1],
@@ -182,7 +203,8 @@ def test_rorxorrolxor():
         assert result
 
 
-def test_complexxor():
+def test_complexxor() -> None:
+    """Test chaining ror and xor with 3 registers."""
     for _ in range(NB_TESTS):
         n = random.randint(1, 10)
         a = random.getrandbits(n)
@@ -196,15 +218,15 @@ def test_complexxor():
         Y1 = QuantumRegister(n, name="Y")
         Z = QuantumRegister(n, name="Z")
 
-        qc = RegisterCircuit(X1, Y1, Z)
-        X2 = qc.ror(X1, r1)
-        qc.xor(X2, Y1)
-        Y2 = qc.ror(Y1, r2)
-        qc.xor(Y2, Z)
-        qc.xor(Z, qc.rol(X2, r3))
+        circuit = RegisterCircuit(X1, Y1, Z)
+        X2 = circuit.ror(X1, r1)
+        circuit.xor(X2, Y1)
+        Y2 = circuit.ror(Y1, r2)
+        circuit.xor(Y2, Z)
+        circuit.xor(Z, circuit.rol(X2, r3))
 
         result = circuit_test(
-            qc,
+            circuit,
             lambda x, y, z: [
                 ror(x, r1, n) ^ y,
                 ror(y, r2, n) ^ z,
@@ -218,7 +240,8 @@ def test_complexxor():
         assert result
 
 
-def test_ripple_add():
+def test_ripple_add() -> None:
+    """Test the ripple-carry adder."""
     for _ in range(NB_TESTS):
         n = random.randint(1, 10)
         a = random.getrandbits(n)
@@ -227,16 +250,17 @@ def test_ripple_add():
         X1 = QuantumRegister(n, name="X")
         Y = QuantumRegister(n, name="Y")
 
-        qc = RegisterCircuit(X1, Y)
+        circuit = RegisterCircuit(X1, Y)
 
-        X2 = qc.add(X1, Y, mode="ripple")
+        X2 = circuit.add(X1, Y, mode="ripple")
 
-        result = circuit_test(qc, lambda x, y: [(x + y) % (2**n), y], [a, b], [X1, Y], [X2, Y])
+        result = circuit_test(circuit, lambda x, y: [(x + y) % (2**n), y], [a, b], [X1, Y], [X2, Y])
 
         assert result
 
 
 def test_lookahead_add():
+    """Test the carry-lookahead adder."""
     for _ in range(NB_TESTS // 5):
         n = random.randint(1, 32)
         a = random.getrandbits(n)
@@ -246,12 +270,12 @@ def test_lookahead_add():
         B = QuantumRegister(n, name="B")
         ancillas = AncillaRegister(RegisterDKRSCarryLookaheadAdder.get_num_ancilla_qubits(n))
 
-        qc = RegisterCircuit(A, B, ancillas)
-        qc.add(A, B, ancillas, mode="lookahead")
+        circuit = RegisterCircuit(A, B, ancillas)
+        circuit.add(A, B, ancillas, mode="lookahead")
 
         if ancillas:
             result = circuit_test(
-                qc,
+                circuit,
                 lambda x, y, _: [(x + y) % (2**n), y, 0],
                 [a, b, 0],
                 [A, B, ancillas],
@@ -260,37 +284,48 @@ def test_lookahead_add():
             )
         else:
             result = circuit_test(
-                qc, lambda x, y: [(x + y) % (2**n), y], [a, b], [A, B], [A, B], verbose=False
+                circuit,
+                lambda x, y: [(x + y) % (2**n), y],
+                [a, b],
+                [A, B],
+                [A, B],
+                verbose=False,
             )
 
         assert result
 
 
-def test_addc():
+def test_addc() -> None:
+    """Test the adder with a constant."""
     for _ in range(NB_TESTS):
         n = random.randint(1, 10)
         a = random.getrandbits(n)
         c = random.getrandbits(n)
         ancillas = AncillaRegister(
-            RegisterConstantDKRSCarryLookaheadAdder.get_num_ancilla_qubits(n)
+            RegisterConstantDKRSCarryLookaheadAdder.get_num_ancilla_qubits(n),
         )
 
         X1 = QuantumRegister(n)
 
-        qc = RegisterCircuit(X1, ancillas)
-        X2 = qc.add(X1, c, ancillas)
+        circuit = RegisterCircuit(X1, ancillas)
+        X2 = circuit.add(X1, c, ancillas)
 
         if ancillas:
             result = circuit_test(
-                qc, lambda x, _: [(x + c) % 2**n, 0], [a, 0], [X1, ancillas], [X2, ancillas]
+                circuit,
+                lambda x, _: [(x + c) % 2**n, 0],
+                [a, 0],
+                [X1, ancillas],
+                [X2, ancillas],
             )
         else:
-            result = circuit_test(qc, lambda x: [(x + c) % 2**n], [a], [X1], [X2])
+            result = circuit_test(circuit, lambda x: [(x + c) % 2**n], [a], [X1], [X2])
 
         assert result
 
 
-def test_alzette():
+def test_alzette() -> None:
+    """Test Alzette against the classical implementation."""
     for _ in range(NB_TESTS):
         n = random.randint(1, 10)
         a = random.getrandbits(n)
@@ -300,16 +335,17 @@ def test_alzette():
         X = QuantumRegister(n)
         Y = QuantumRegister(n)
 
-        qc = QuantumCircuit(X, Y)
+        circuit = QuantumCircuit(X, Y)
         gate = Alzette(X, Y, c)
-        qc.append(gate, chain(*gate.outputs))
+        circuit.append(gate, chain(*gate.outputs))
 
-        result = circuit_test(qc, lambda x, y: c_alzette(x, y, c, n), [a, b], [X, Y], [X, Y])
+        result = circuit_test(circuit, lambda x, y: c_alzette(x, y, c, n), [a, b], [X, Y], [X, Y])
 
         assert result
 
 
-def test_ctraxl():
+def test_ctraxl() -> None:
+    """Test the classical TRAX-L against values pre-computed with the C reference implementation."""
     x = [0xAEA9F4E8, 0xC926A22F, 0x9D1078F8, 0x8A779F98]
     y = [0x24E002DC, 0x8F34F225, 0x13FF3742, 0x510E85EA]
 
@@ -334,7 +370,8 @@ def test_ctraxl():
     assert res_y == true_y
 
 
-def test_traxm():
+def test_traxm() -> None:
+    """Test TRAX-M against the classical implementation."""
     for _ in range(NB_TESTS // 10):
         n = random.choice([16, 32, 64, 128])
         x = [random.getrandbits(n // 4) for _ in range(2)]
@@ -344,14 +381,14 @@ def test_traxm():
         X = [QuantumRegister(n // 4, name=f"X{i}") for i in range(2)]
         Y = [QuantumRegister(n // 4, name=f"Y{i}") for i in range(2)]
         K = [QuantumRegister(n // 4, name=f"K{i}") for i in range(4)]
-        ancillas = AncillaRegister(Traxm_enc_round.get_num_ancilla_qubits(n))
+        ancillas = AncillaRegister(TraxmEncRound.get_num_ancilla_qubits(n))
 
-        qc = RegisterCircuit(*X, *Y, *K, ancillas)
-        gate = Traxm_enc_round(X, Y, K, None, ancillas)
-        qc.append(gate, list(chain(*gate.inputs)))
+        circuit = RegisterCircuit(*X, *Y, *K, ancillas)
+        gate = TraxmEncRound(X, Y, K, None, ancillas)
+        circuit.append(gate, list(chain(*gate.inputs)))
 
         result = circuit_test(
-            qc,
+            circuit,
             lambda *params: chain.from_iterable(
                 c_traxm_enc(
                     list(params[0:2]),
@@ -361,7 +398,7 @@ def test_traxm():
                     n=n // 4,
                     nsteps=1,
                     final_addition=False,
-                )
+                ),
             ),
             x + y + round_key,
             gate.inputs[:-1],
@@ -371,7 +408,8 @@ def test_traxm():
         assert result
 
 
-def test_traxl():
+def test_traxl() -> None:
+    """Test TRAX-L against the classical implementation."""
     # This test is really slow so we must run it less times
     for _ in range(1):
         n = random.choice([16, 32, 64, 128, 256])
@@ -383,14 +421,14 @@ def test_traxl():
         X = [QuantumRegister(n // 8, name=f"X{i}") for i in range(4)]
         Y = [QuantumRegister(n // 8, name=f"Y{i}") for i in range(4)]
         K = [QuantumRegister(n // 8, name=f"K{i}") for i in range(8)]
-        ancillas = AncillaRegister(Traxl_enc.get_num_ancilla_qubits(n))
+        ancillas = AncillaRegister(TraxlEnc.get_num_ancilla_qubits(n))
 
-        qc = RegisterCircuit(*X, *Y, *K, ancillas)
-        gate = Traxl_enc(X, Y, K, tweak, ancillas)
-        qc.append(gate, list(chain(*gate.inputs)))
+        circuit = RegisterCircuit(*X, *Y, *K, ancillas)
+        gate = TraxlEnc(X, Y, K, tweak, ancillas)
+        circuit.append(gate, list(chain(*gate.inputs)))
 
         result = circuit_test(
-            qc,
+            circuit,
             lambda *params: [
                 a
                 for sublist in c_traxl_enc(
@@ -408,7 +446,3 @@ def test_traxl():
         )
 
         assert result
-
-
-if __name__ == "__main__":
-    test_ripple_add()
